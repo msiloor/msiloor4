7ceafcb0901d50ca444c081c0db4d39796a5ec2e
昕羽 <msiloor@oitlook.com>
2022-03-07 03:52:50 +0800

    TS

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+
+import { createApp } from 'vue'
+import element from "element-plus"
+import 'element-plus/dist/index.css'
+
+import App from './App.vue'
+
+
+createApp(App).use(element).mount('#app')
